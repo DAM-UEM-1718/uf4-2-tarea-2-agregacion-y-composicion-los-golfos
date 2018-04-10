89c5cda2de78740d21e8c997a0cf2a4c325b9c7b
hernan <hernanagf97@gmail.com>
2018-04-10 16:29:05 +0200

    Enunciado 3

---
+++ src/Enunciado_3/Pagina.java
@@ -0,0 +1,29 @@
+package Enunciado_3;
+
+public class Pagina {
+
+	private String contenido;
+	private int numero;
+	
+	public void Pagina(String content, int numero) {
+		
+	}
+
+	public String getContenido() {
+		return contenido;
+	}
+
+	public void setContenido(String contenido) {
+		this.contenido = contenido;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	
+	
+}
